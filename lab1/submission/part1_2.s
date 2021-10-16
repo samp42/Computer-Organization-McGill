4821b9cff353bd3427457a4b4b8a2ffe78c10202
Samuel Proulx <samuelproulx26@gmail.com>
2021-10-16 14:03:54 -0400

    lab 1 submission

---
+++ lab1/submission/part1_2.s
@@ -0,0 +1,26 @@
+.text
+    n: .word 8 // variable for fibonacci's number to find
+.global _start
+ _start:
+
+     mov a1, #0
+     mov a3, #0
+     mov a2, #1
+     ldr a4, n
+     sub a4, a4, #1
+
+     push {lr}
+
+ @@ -21,19 +20,29 @@ _start:
+     b end
+
+ fib:
+     @ compute sum of 2 previous numbers in a1
+     add a1, a2, a3
+     @ n-2 <- n-1
+     mov a3, a2
+     @ n-1 <- n
+     mov a2, a1
+
+     subs a4, a4, #1
+     bgt fib
