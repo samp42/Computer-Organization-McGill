ae1a7368e0a4f2d19691aad17ef1ac38034520ec
Samuel Proulx <samuelproulx26@gmail.com>
2021-11-10 10:22:53 -0500

    start game overview

--- lab3/game.s
+++ lab3/game.s
@@ -12,12 +12,31 @@
 .equ PS2_DATA, 0xFF200100
 .equ PS2_CONTROL, 0xFF200104
 
+// colors
+.equ WHITE, 0xFFFF
+.equ BLACK, 0x0000
+.equ GREEN, 0x07E0
+
 .text
 .global _start
 _start:
-        bl      input_loop
-end:
-        b       end
+
+    // fill screen with color
+
+    // draw 207x207 px grid
+
+    // game starts on '0' keyboard input
+
+    // enter game loop
+
+GAME_LOOP:
+    // write whose turn it is at top-center
+
+    // wait for player input and display it (X / O)
+
+    // repeat for other player
+
+    // when game over, display result
 
 // ------------------------------------------------------------------------------------
 // SUBROUTINES
@@ -123,57 +142,6 @@ EXIT_PS_DATA:
 	POP {R4-R5}
 	BX LR
 
-
-// ------------------------------------------------------------------------------------
-// SUBROUTINES
-// ------------------------------------------------------------------------------------
-
-write_hex_digit:
-	push    {r4, lr}
-	cmp     r2, #9
-	addhi   r2, r2, #55
-	addls   r2, r2, #48
-	and     r2, r2, #255
-	bl      VGA_write_char_ASM
-	pop     {r4, pc}
-write_byte:
-	push    {r4, r5, r6, lr}
-	mov     r5, r0
-	mov     r6, r1
-	mov     r4, r2
-	lsr     r2, r2, #4
-	bl      write_hex_digit
-	and     r2, r4, #15
-	mov     r1, r6
-	add     r0, r5, #1
-	bl      write_hex_digit
-	pop     {r4, r5, r6, pc}
-input_loop:
-	push    {r4, r5, lr}
-	sub     sp, sp, #12
-	bl      VGA_clear_pixelbuff_ASM
-	bl      VGA_clear_charbuff_ASM
-	mov     r4, #0
-	mov     r5, r4
-	b       .input_loop_L9
-.input_loop_L13:
-	ldrb    r2, [sp, #7]
-	mov     r1, r4
-	mov     r0, r5
-	bl      write_byte
-	add     r5, r5, #3
-	cmp     r5, #79
-	addgt   r4, r4, #1
-	movgt   r5, #0
-.input_loop_L8:
-	cmp     r4, #59
-	bgt     .input_loop_L12
-.input_loop_L9:
-	add     r0, sp, #7
-	bl      read_PS2_data_ASM
-	cmp     r0, #0
-	beq     .input_loop_L8
-	b       .input_loop_L13
-.input_loop_L12:
-	add     sp, sp, #12
-	pop     {r4, r5, pc}
+END:
+    B END
+.end
--- lab3/line.s
+++ lab3/line.s
@@ -1,49 +1,137 @@
+.data
+// pixel buffer
+.equ PIX_BUFFER, 0xC8000000
+.equ PIX_BUFFER_WIDTH, 319		// x
+.equ PIX_BUFFER_HEIGHT, 239		// y
+
+// character buffer
+.equ CHAR_BUFFER, 0xC9000000
+.equ CHAR_BUFFER_WIDTH, 79		// x
+.equ CHAR_BUFFER_HEIGHT, 59		// y
+
+.text
 .global _start
 _start:
 
-
-        ldr     r3, .colors+8
-        str     r3, [sp]
-        mov     r3, #120
-        mov     r2, #214
-        mov     r1, r3
-        mov     r0, #106
-        bl      draw_rectangle
+	ldr     r3, .colors+8
+	str     r3, [sp]
+	mov     r3, #120
+	mov     r2, #214
+	mov     r1, r3
+	mov     r0, #106
+	bl      draw_rectangle
 
 end: b end
 
-@ TODO: copy VGA driver here.
+@ draws a point of specified color at specfied location
+@ R0: x coordinate
+@ R1: y coordinate
+@ R2: color
+VGA_draw_point_ASM:
+	PUSH {R0-R1, R4}
+	LDR R4, =PIX_BUFFER
+	LSL R0, #1
+	LSL R1, #10
+	ADD R0, R1
+	ADD R0, R4
+	STRH R2, [R0]
+	POP {R0-R1, R4}
+	BX LR
+
+
+@ sets every pixel in pixel buffer to 0 (black screen)
+VGA_clear_pixelbuff_ASM:
+	PUSH {R0-R2}
+	LDR R0, =PIX_BUFFER_WIDTH
+	LDR R1, =PIX_BUFFER_HEIGHT
+	MOV R2, #0
+
+PIX_BUFFER_CLEAR_LOOP:
+	
+	PUSH {LR}
+	BL	VGA_draw_point_ASM
+	POP {LR}
+	
+	SUBS R0, #1
+	LDRLT R0, =PIX_BUFFER_WIDTH
+	SUBLTS R1, #1
+	LDRLT R1, =PIX_BUFFER_HEIGHT
+	BLT PIX_BUFFER_CLEAR_END
+	B PIX_BUFFER_CLEAR_LOOP
+
+PIX_BUFFER_CLEAR_END:
+	POP {R0-R2}
+	BX LR
+
+
+@ writes character to given location in character buffer
+@ R0: x coordinate
+@ R1: y coordinate
+@ R2: ASCII code of character
+VGA_write_char_ASM:
+	PUSH {R0-R1, R4}
+	LDR R4, =CHAR_BUFFER
+	LSL R1, #7
+	ADD R0, R1
+	ADD R0, R4
+	STRB R2, [R0]
+	POP {R0-R1, R4}
+	BX LR
+
+
+@ sets every character in character buffer to 0
+VGA_clear_charbuff_ASM:
+	PUSH {R0-R2}
+	LDR R0, =CHAR_BUFFER_WIDTH
+	LDR R1, =CHAR_BUFFER_HEIGHT
+	MOV R2, #0
+
+CHAR_BUFFER_CLEAR_LOOP:	
+	PUSH {LR}
+	BL	VGA_write_char_ASM
+	POP {LR}
+	
+	SUBS R0, #1
+	LDRLT R0, =CHAR_BUFFER_WIDTH
+	SUBLTS R1, #1
+	LDRLT R1, =CHAR_BUFFER_HEIGHT
+	BLT CHAR_BUFFER_CLEAR_END
+	B CHAR_BUFFER_CLEAR_LOOP
+
+CHAR_BUFFER_CLEAR_END:
+	POP {R0-R2}
+	BX LR
 
 
 .colors:
-        .word   2911
-        .word   65535
-        .word   45248
+	.word   2911
+	.word   65535
+	.word   45248
 
 draw_rectangle:
-        push    {r4, r5, r6, r7, r8, r9, r10, lr}
-        ldr     r7, [sp, #32]
-        add     r9, r1, r3
-        cmp     r1, r9
-        popge   {r4, r5, r6, r7, r8, r9, r10, pc}
-        mov     r8, r0
-        mov     r5, r1
-        add     r6, r0, r2
-        b       .line_L2
+	push    {r4, r5, r6, r7, r8, r9, r10, lr}
+	ldr     r7, [sp, #32]
+	add     r9, r1, r3
+	cmp     r1, r9
+	popge   {r4, r5, r6, r7, r8, r9, r10, pc}
+	mov     r8, r0
+	mov     r5, r1
+	add     r6, r0, r2
+	b       .line_L2
 .line_L5:
-        add     r5, r5, #1
-        cmp     r5, r9
-        popeq   {r4, r5, r6, r7, r8, r9, r10, pc}
+	add     r5, r5, #1
+	cmp     r5, r9
+	popeq   {r4, r5, r6, r7, r8, r9, r10, pc}
 .line_L2:
-        cmp     r8, r6
-        movlt   r4, r8
-        bge     .line_L5
+	cmp     r8, r6
+	movlt   r4, r8
+	bge     .line_L5
 .line_L4:
-        mov     r2, r7
-        mov     r1, r5
-        mov     r0, r4
-        bl      VGA_draw_point_ASM
-        add     r4, r4, #1
-        cmp     r4, r6
-        bne     .line_L4
-        b       .line_L5
+	mov     r2, r7
+	mov     r1, r5
+	mov     r0, r4
+	bl      VGA_draw_point_ASM
+	add     r4, r4, #1
+	cmp     r4, r6
+	bne     .line_L4
+	b       .line_L5
